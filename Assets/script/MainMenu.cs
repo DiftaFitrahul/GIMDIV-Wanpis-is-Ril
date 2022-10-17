bc14f7800c862e429bc3fa94eeaab86529a1cdad
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2022-10-17 15:25:15 +0700

    Adding Main Menu (not finished yet)
    Iam still build main menu page from yt tutorial https://youtu.be/-GWjA6dixV4

---
+++ Assets/script/MainMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void playGame(){
+        SceneManager.LoadScene("Playing_Game");
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
